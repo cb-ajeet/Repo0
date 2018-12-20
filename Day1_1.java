f874530532b4e2b71949811321ca747847494d2d
ajeet k <ajeet.k@chargebee.com>
2018-12-20 17:33:49 +0530

    AT 5:33 , will try to squash

--- Day1_1.java
+++ Day1_1.java
@@ -1,5 +1,6 @@
 public class Day1_1{
 public static void main(String []args){
+System.out.println("Trying to squash at 5:26 Dec 20, 2018");
 System.out.println("Trying for test Branch");
 System.out.println("Hello World! Greetings from Ajeet on Thursday Morning");
 }
